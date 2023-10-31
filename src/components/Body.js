fac8284aed59aeeac15da334c36eda8262290066
kuraharitha@gmail.com <haritha-2726>
2023-10-30 22:59:57 -0700

    background image chnages

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import Main from './Main'
+import  back from "../images/backgroundimage.jpeg";
 
 const Body = () => {
+  const mainStyle = {
+    backgroundImage: `url(${back})`,
+    
+    backgroundImage: `url(${back})`,
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover', // Corrected property name
+    minHeight: '100vh',  
+  };
   return (
-    <div id="section-1" className='back content'> 
+    <div id="section-1" className=' content' style={mainStyle}> 
     <Main/>
     </div>
   )
