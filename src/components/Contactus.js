517b52c164de28c86623a6e6639e3c9f52de2080
kuraharitha@gmail.com <haritha-2726>
2023-11-04 23:51:41 -0700

    updated contact us

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -1,25 +1,68 @@
 import React from 'react';
-import Form from './Form';
-import Address from './Address';
+// import Form from './Form';
+// import Address from './Address';
+import { TbMapSearch } from "react-icons/tb";
+import { FiPhoneCall } from "react-icons/fi";
+import { TfiEmail } from "react-icons/tfi";
 
 const Contactus = () => {
   return (
-    <div  id="section-4" className="     flex justify-center items-center h-screen">
-      <div className="p-5">
-        <h1    className=" font-thin text-2xl md:text-5xl text-center p-4 pb-7" style={{ textShadow: '2px 2px 4px rgba(255, 165, 0, 0.7)' }}>Contact Us</h1>
-        <div className="flex flex-col md:flex-row items-center pt-7 justify-center"> {/* Center content and form */}
-          <div className="w-full md:w-1/2 lg:w-1/3 p-4 text-center">
-            <p className="text-2xl font-bold mb-4">How Can We Help You?</p>
-            <p className="text-gray-600 text-xl">Our team loves to solve your challenges and would be delighted to chat with you about your project and opportunities.</p>
-          </div>
-                 <div class="w-full md:w-1/2 lg:w-[35%] p-4 bg-white rounded-lg shadow-xl mt-4 md:mt-0">
-                              <Form />
-                   </div>
-
+    <>
+    <h1 id="section-4" className="font-thin text-2xl md:text-5xl text-center p-10  " style={{ textShadow: '2px 2px 4px rgba(255, 165, 0, 0.7)' }}>
+         Contact Us
+        </h1>
+    <div class="contact-container pb-10">
+  
+    <div class="card">
+  <div class="card-content">
+  
+    <h1 className=' text-orange-700'>Send us a message</h1>
+    <p>Our team loves to solve your challenges and delighted to chat with you about your project and opportunities</p>
+    <form>
+      <div class="input-group">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
         </div>
-        <Address />
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
       </div>
-    </div>
+      <div>
+        <label className='pt-2' for="message">Message:</label>
+        <textarea id="message"   name="message" required></textarea>
+      </div>
+      <button type="submit" className="send-button  m-4">Send</button>
+    </form>
+  </div>
+  <div class="card-info">
+ 
+  <div className="p-4 bg-orange text-white">
+  <h2 className="text-2xl font-bold mb-2">Contact Information</h2>
+  <div className="flex items-center mb-2">
+    <TbMapSearch className="text-2xl mr-2" />
+    <p>2108 N ST, California, 95816</p>
+  </div>
+  <div className="flex items-center mb-2">
+    <FiPhoneCall className="text-2xl mr-2" />
+    <p>Phone: (650) 753-7333</p>
+  </div>
+  <div className="flex items-center mb-2">
+    <TfiEmail className="text-2xl mr-2" />
+    <p>Email: hr@mdis.tech</p>
+  </div>
+  <a href="https://www.linkedin.com/" target="blank" className="linkedin-link">Follow us on LinkedIn</a>
+</div>
+
+  </div>
+</div>
+</div>
+
+
+
+
+    </>
   );
 };
 
